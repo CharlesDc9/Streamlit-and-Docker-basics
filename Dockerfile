11249053bdf78723e096e8d11e238777b42ba19f
CharlDc9 <charldec99@gmail.com>
2024-09-20 17:22:19 +0200

    ajouts des détails

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@
 FROM python:3.10-slim
 WORKDIR /app
 COPY . .
-RUN pip install streamlit pandas numpy
-CMD ["streamlit", "run", "Hello.py", "--server.port=8501", "--server.address=0.0.0.0"]
+RUN pip install --no-cache-dir streamlit pandas matplotlib scikit-learn
 EXPOSE 8501
+CMD ["streamlit", "run", "Hello.py", "--server.port=8501", "--server.address=0.0.0.0"]
--- Hello.py
+++ Hello.py
@@ -5,24 +5,16 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.write("# Welcome to Streamlit! 👋")
+st.write("# Welcome to my try on a (dockerized) Streamlit app ! 👋")
 
 st.sidebar.success("Select a demo above.")
 
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    Don't try to find any links between all the pages, this is just
+    a test to see how Streamlit works. You can find enclosed in my Github repository
+    (https://github.com/CharlesDc9/Streamlit-and-Docker-basics) the code ! 
+
+    Have fun ! 🎉
 """
 )
